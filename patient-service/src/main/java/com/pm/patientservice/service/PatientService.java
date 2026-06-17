69e5b908335648d9428f12da47b7fe4db5de918e
THARANIPRAKASH <tharaniprakashp2006@gmail.com>
2026-06-17 19:32:08 +0530

    create controller for patient and implement service method

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,11 +1,14 @@
 package com.pm.patientservice.service;
 
 import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class PatientService {
     private PatientRepository patientRepository;
 
@@ -15,5 +18,10 @@ public class PatientService {
 
     public List<PatientResponseDTO> getPatients(){
         List<Patient> patients = patientRepository.findAll();
+
+        List<PatientResponseDTO> patientResponseDTOs = patients.stream()
+                .map(patient -> PatientMapper.toDTO(patient)).toList();
+
+        return patientResponseDTOs;
     }
 }
